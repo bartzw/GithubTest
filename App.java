7a045db1cc1f28c525d898f138e8072e5c3c7187
unknown <Bart@LaptopBart.bartzwaagstra.nl>
2019-07-23 14:07:26 +0200

    add method doStuff()

--- App.java
+++ App.java
@@ -2,4 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("App.main");
     }
+    private void doStuff() {
+        System.out.println("Feature X: Do stuff");
+    }
 }
